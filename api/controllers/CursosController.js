be897798ab9fd09181e4ae5eb07da6bc785c6584
Saul Navarrov <sinavarrov@gmail.com>
2016-12-20 11:44:22 -0500

    Api Generada para cursos
    controlador y modelo

---
+++ api/controllers/CursosController.js
@@ -0,0 +1,11 @@
+/**
+ * CursosController
+ *
+ * @description :: Server-side logic for managing cursos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+

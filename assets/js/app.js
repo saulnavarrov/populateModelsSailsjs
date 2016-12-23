f775792e5f748a8228069d175d05b88788ee80f4
Saul Navarrov <sinavarrov@gmail.com>
2016-12-22 19:15:18 -0500

    Identacion de asociaciones
    Identacion de la asociacion de la lista de profesores con sus respectivas clases

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,22 +14,35 @@ $(function () {
       'data': {},
       'headers': {}
     }, (data, res) => {
-      console.log(data);
-      console.log(res);
+      // console.log(data);
+      // console.log(res);
       // en caso de que no halla un problema este lo pueda resolver
       if(res.statusCode !== 200){
         console.error(res.error);
       }else{
+          relationTeacherCursos.html(`<ul id="listRel"></ul>`);
 
-        data.forEach(it => {
-          printToDisplayDashboardList(it);
+          data.forEach(itT => {
+            printToDisplayDashboardList(itT);
+
+            console.log(itT.cursos);
+            itT.cursos.forEach(itC => {
+              printToDisplayDashboardListSub(itC, itT);
+            });
+
         });
       }
     });
   }
 
-  function printToDisplayDashboardList(d) {
-    console.log(d);
+  function printToDisplayDashboardList(dT) {
+    let list = $('#relationTeacherCursos ul#listRel');
+    list.append(`<li id="liT-${dT.id}">${dT.name} ${dT.documentId} <ul id="listC-${dT.id}"></ul> </li>`);
+  }
+
+  function printToDisplayDashboardListSub(dC, dT) {
+    let list = $(`li ul#listC-${dT.id}`);
+    list.append(`<li id="liC-${dC.id}">${dC.nameCurso}</li>`);
   }
 });
 

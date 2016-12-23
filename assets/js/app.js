4943f0e3a19b0a23b6c8e0962c44ed93847ecc30
Saul Navarrov <sinavarrov@gmail.com>
2016-12-22 19:17:55 -0500

    agregando comentarios
    agregando comentario y cantidad de cursos que tienen inscritos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
 $(function () {
   var urlPaths = window.location.pathname; // url actual
 
+  // Iniciación de la lista en dahsboard
   if(urlPaths === '/'){
     getListAsotiationTC();
   }
 
+  /**
+   * [getListAsotiationTC description]
+   * @return {[type]} [description]
+   */
   function getListAsotiationTC(){
     let relationTeacherCursos = $('#relationTeacherCursos');
 
@@ -35,11 +40,22 @@ $(function () {
     });
   }
 
+  /**
+   * [printToDisplayDashboardList description]
+   * @param  {[type]} dT [description]
+   * @return {[type]}    [description]
+   */
   function printToDisplayDashboardList(dT) {
     let list = $('#relationTeacherCursos ul#listRel');
-    list.append(`<li id="liT-${dT.id}">${dT.name} ${dT.documentId} <ul id="listC-${dT.id}"></ul> </li>`);
+    list.append(`<li id="liT-${dT.id}">${dT.name} ${dT.documentId} ${dT.cursos.length} <ul id="listC-${dT.id}"></ul> </li>`);
   }
 
+  /**
+   * [printToDisplayDashboardListSub description]
+   * @param  {[type]} dC [description]
+   * @param  {[type]} dT [description]
+   * @return {[type]}    [description]
+   */
   function printToDisplayDashboardListSub(dC, dT) {
     let list = $(`li ul#listC-${dT.id}`);
     list.append(`<li id="liC-${dC.id}">${dC.nameCurso}</li>`);
